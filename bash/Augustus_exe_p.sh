f78a6c5d2e7c1bae770b260f3542cf0257005f76
yoshiakifujita1973 <yoshiakifujita1973@gmail.com>
2022-12-02 22:55:16 -0500

    Add files via upload

---
+++ bash/Augustus_exe_p.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+augustus --species=human --UTR=on myasmb_p068_1.contig > myasmb_p068_1_augustus.txt
+augustus --species=human --UTR=on myasmb_p068_2.contig > myasmb_p068_2_augustus.txt
+augustus --species=human --UTR=on myasmb_p068_3.contig > myasmb_p068_3_augustus.txt
+
